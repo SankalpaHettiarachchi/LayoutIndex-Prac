f718620469f3210714143e40080219998f96150a
DevCada <itt1819041@tec.rjt.ac.lk>
2025-04-23 20:25:43 +0530

    Bug fix view order

--- resources/js/Pages/Orders/viewOrder.jsx
+++ resources/js/Pages/Orders/viewOrder.jsx
@@ -132,12 +132,6 @@ export default function ViewOrder({ auth, order }) {
                                     >
                                         Back to Orders
                                     </Link>
-                                    <Link
-                                        href={route('orders.edit', order.id)}
-                                        className="px-4 py-2 text-sm font-medium text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                    >
-                                        Edit Order
-                                    </Link>
                                 </div>
                             </div>
                         </div>
